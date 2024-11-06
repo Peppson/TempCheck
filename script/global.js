69f60326ec52c4456f5a37e7068556fdd1625a50
Joakim Bjerselius <176925453+jockeBjers@users.noreply.github.com>
2024-11-06 22:31:50 +0100

    add svg icons instead of the weathermap icons

--- script/global.js
+++ script/global.js
@@ -20,13 +20,73 @@ function WeatherData() {
 function displayWeatherData(weatherData) {
     const infoText = document.getElementById("display-info");
     const iconCode = weatherData.weather[0].icon;
-    const iconUrl = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
     const temp = Math.round(weatherData.main.temp);
+    const iconPath = getSVGIcon(iconCode);
 
     infoText.innerHTML = `
     <div class="icon-info">
-    <div>  ${weatherData.name}, ${weatherData.sys.country} </div>
-    <img src="${iconUrl}" style="width: 160px; height: 160px;">
-    <div>${weatherData.weather[0].description}</div></div>
-    <div class="temp-info"> ${temp}° </div>`;
+        <div>${weatherData.name}, ${weatherData.sys.country}</div>
+        <img src="${iconPath}" width="250px" height="250px">
+    </div>
+    <div class="temp-info">${temp}°C</div>`;
+}
+
+function getSVGIcon(iconCode) {
+    let iconPath;
+    switch (iconCode) {
+        case '01d': 
+            iconPath = 'icons/animated/day.svg';
+            break;
+        case '01n': 
+            iconPath = 'icons/animated/night.svg';
+            break;
+        case '02d': 
+            iconPath = 'icons/animated/cloudy-day-1.svg';
+            break;
+        case '02n': 
+            iconPath = 'icons/animated/cloudy-night-1.svg';
+            break;
+        case '03d': 
+            iconPath = 'icons/animated/cloudy-day-2.svg';
+            break;
+        case '03n': 
+            iconPath = 'icons/animated/cloudy-night-2.svg';
+            break;
+        case '04d':
+            iconPath = 'icons/animated/cloudy-day-3.svg';
+            break;
+        case '04n': 
+            iconPath = 'icons/animated/cloudy-night-3.svg';
+            break;
+        case '09d': 
+            iconPath = 'icons/animated/rainy-1.svg';
+            break;
+        case '09n': 
+            iconPath = 'icons/animated/rainy-4.svg';
+            break;
+        case '10d': 
+            iconPath = 'icons/animated/rainy-2.svg';
+            break;
+        case '10n': 
+            iconPath = 'icons/animated/rainy-5.svg';
+            break;
+        case '11d': 
+        case '11n':
+            iconPath = 'icons/animated/thunder.svg';
+            break;
+        case '13d': 
+            iconPath = 'icons/animated/snowy-1.svg';
+            break;
+        case '13n':
+            iconPath = 'icons/animated/snowy-5.svg';
+            break;
+        case '50d': 
+        case '50n': 
+            iconPath = 'icons/animated/cloudy.svg';
+            break;
+        default:
+            iconPath = 'icons/animated/cloudy.svg';
+            break;
+    }
+    return iconPath;
 }
